338ac6e1e55e95acbd5d56e31b686d67232ace5e
justinmo128 <j.mo1@share.epsb.ca>
2023-09-05 14:23:51 -0600

    finish assignment

--- main.js
+++ main.js
@@ -14,21 +14,27 @@ function calcGrades() {
     let finalGrade = (grade1 + grade2 + grade3 + grade4 + grade5) / 5;
 
     // Output
-    // Does not check if grade1 through grade5 is between 0 and 100...
-    // ..because that would be too much typing.
+    document.getElementById("grade-out").innerHTML = finalGrade;
     if (finalGrade <= 10) {
-        document.getElementById("grade-out").innerHTML = finalGrade
-        document.getElementById("extra-message").innerHTML = "noob"
+        document.getElementById("extra-message").innerHTML = "noob";
     } 
     else if (finalGrade >= 95) {
-        document.getElementById("grade-out").innerHTML = finalGrade
-        document.getElementById("extra-message").innerHTML = "Good job!"
-    } 
-    else if (finalGrade < 95 && finalGrade > 10) {
-        document.getElementById("grade-out").innerHTML = finalGrade
-    } 
-    else {
-        document.getElementById("grade-out").innerHTML = "Invalid"
-        document.getElementById("extra-message").innerHTML = "Check to make sure you inputted your values correctly."
+        document.getElementById("extra-message").innerHTML = "Good job!";
+    } else {
+        document.getElementById("extra-message").innerHTML = "";
     }
+    document.getElementById("letter-grade-1").innerHTML = getLetterGrade(grade1);
+    document.getElementById("letter-grade-2").innerHTML = getLetterGrade(grade2);
+    document.getElementById("letter-grade-3").innerHTML = getLetterGrade(grade3);
+    document.getElementById("letter-grade-4").innerHTML = getLetterGrade(grade4);
+    document.getElementById("letter-grade-5").innerHTML = getLetterGrade(grade5);
+    document.getElementById("letter-grade-final").innerHTML = getLetterGrade(finalGrade);
+}
+
+function getLetterGrade(percentage) {
+    if (percentage >= 90) return "A";
+    else if (percentage >= 75) return "B";
+    else if (percentage >= 60) return "C";
+    else if (percentage >= 50) return "D";
+    else return "F";
 }
